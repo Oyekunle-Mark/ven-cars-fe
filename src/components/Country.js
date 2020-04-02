e20d98ef79a5a266c3f8d0b5dd4ae04f24c71df0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-02 14:27:35 +0100

    Styled the country

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,13 +3,11 @@ import Prop from 'prop-types';
 import styled from 'styled-components';
 
 const StyledCountry = styled.span`
-  display: flex;
-  flex-direction: row;
-
-  font-size: 16px;
+  font-size: 13px;
   background: #dedede;
-  border-radius: 8px;
+  border-radius: 12px;
   padding: 5px 10px;
+  margin: 0 5px;
 `;
 
 const Country = ({ country }) => <StyledCountry>{country}</StyledCountry>;
