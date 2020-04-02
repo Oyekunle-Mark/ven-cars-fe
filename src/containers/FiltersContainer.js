9f5b04bfd4b7eba1e3ab0fcb0e1b98b21537181a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-02 10:22:25 +0100

    The filter component is wrapped in a link tag

--- src/containers/FiltersContainer.js
+++ src/containers/FiltersContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Prop from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import Filter from '../components/Filter';
 
@@ -7,7 +8,9 @@ const FiltersContainer = ({ filters }) => {
   return (
     <div>
       {filters.map(filter => (
-        <Filter key={filter.id} {...filter} />
+        <Link key={filter.id} to={`/filter/${filter.id}`}>
+          <Filter {...filter} />
+        </Link>
       ))}
     </div>
   );
