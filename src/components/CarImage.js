4770ffa8c231c923e5e7c40dafad80af5532d605
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-02 17:21:22 +0100

    The car image returns the car svg

---
+++ src/components/CarImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import carSvg from '../assets/car.svg';
+
+const CarImage = () => (
+  <div>
+    <img src={carSvg} alt="car" />
+  </div>
+);
+
+export default CarImage;
