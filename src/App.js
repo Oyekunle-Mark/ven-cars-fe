77818e74d6aaaa041be4b58ec883a8e181079b7b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-02 01:39:43 +0100

    Sets up the state and fetch callbacl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div>
-      Get started!
-    </div>
-  );
+  const [carState, setCarState] = useState({
+    loading: true,
+    filters: [],
+  });
+
+  const fetchFilters = useCallback(async () => {
+    const response = await axios.get(
+      'https://ven-cars.herokuapp.com/api/v1/filters',
+    );
+
+    setCarState({
+      loading: false,
+      filters: response.data,
+    });
+  });
+
+  return <div>Get started!</div>;
 }
 
 export default App;
