b562fa9e533581f201561c72f32e1f3e4aa0deec
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-02 14:47:00 +0100

    Created a color map

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Prop from 'prop-types';
 
+const colorMap = {
+  green: '#50af55',
+  violet: '#9c2fae',
+  yellow: '#fcc12f',
+  blue: '#4153af',
+  teal: '#12807f',
+  maroon: '#ad3260',
+  red: '#ec2b26',
+  aquamarine: '#84fed5',
+  orange: '#fb572f',
+  mauv: '#b685a5',
+};
+
 const Color = ({ color }) => <span>{color}</span>;
 
 Color.propTypes = {
