e5a02396a69ade2177ebd3bfe9d856279c0119ac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-02 10:40:58 +0100

    Created a memoized function

--- src/containers/CarOwnersContainer.js
+++ src/containers/CarOwnersContainer.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 const CarOwnersContainer = () => {
+  const [cars, setCars] = useState({
+    loading: true,
+    filteredCars: [],
+  });
+
   const { id } = useParams();
+
+  const fetchCars = useCallback(async () => {
+    const response = await axios.get(
+      `https://ven-cars.herokuapp.com/api/v1/filter/${id}`,
+    );
+
+    setCars({
+      loading: false,
+      filters: response.data.data,
+    });
+  });
 
   return <div>This is a car owner container for filter {id}</div>;
 };
